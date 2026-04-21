b43dd17f4b153c7ee3d84d19ce598fea9287dc46
sc-forlife <salemsc7@gmail.com>
2026-04-21 09:37:32 +0200

    Testing and analysing products from API

---
+++ frontend/src/api/testing.js
@@ -0,0 +1,9 @@
+export async function testingApi() {
+  try {
+    const response = await fetch("https://dummyjson.com/products/?limit=0");
+    const responseData = await response.json();
+    return responseData.products.length;
+  } catch (err) {
+    console.log(err);
+  }
+}
