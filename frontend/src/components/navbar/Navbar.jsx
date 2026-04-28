15c3764a49a3f34f9db9f440b288bae6c21b9534
sc-forlife <salemsc7@gmail.com>
2026-04-28 20:16:19 +0200

    Add auto suggestion bar for the search bar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,21 +1,34 @@
 import css from "./Navbar.module.css";
+import { numbersInWords } from "../../data/dummyData";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [display, setDisplay] = useState(false);
+  function suggestionsBar() {
+    setDisplay(true);
+  }
+
   return (
     <>
       <div className={css.navbar_container}>
         <div id={css.navbar_side1}>
           <div id={css.logo_search}>
-            <h1>Logo</h1>
+            <h1 id={css.logo}>Logo</h1>
             <div id={css.search}>
-              <div>
-                <input type="text" placeholder="Enter text" />
-                <button>search</button>
-              </div>
+              <input
+                onFocus={suggestionsBar}
+                type="text"
+                placeholder="Enter text"
+              />
+              <button>search</button>
+              {display ? <div id={css.suggestionsBar}></div> : null}
             </div>
           </div>
-          <div>
-            <button id={css.category_button}>category</button>
+          <div id={css.category}>
+            <button id={css.category_button}>category One</button>
+            <button id={css.category_button}>category Two</button>
+            <button id={css.category_button}>category Three</button>
+            <button id={css.category_button}>category Four</button>
           </div>
         </div>
         <div id={css.navbar_side2}>
