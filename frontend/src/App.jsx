30ef099536e4f110c69c2769ebd7683df5a7fd19
sc-forlife <salemsc7@gmail.com>
2026-05-13 16:47:40 +0200

    Saving main to get ready to transfere to new branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import { createContext } from "react";
 import Home from "./pages/home/Home";
-import ProductImage from "./components/ProductImage/ProductImage"; // Testing Component
 
 export default function App() {
   return (
     <>
-      <ProductImage />
+      <Home />
     </>
   );
 }
