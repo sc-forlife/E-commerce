1f5bf43da95283970545dc07958cbf7ee7bbd09b
sc-forlife <salemsc7@gmail.com>
2026-05-13 17:01:43 +0200

    Completed the designing of ProductImage component

--- frontend/src/components/ProductImage/ProductImage.jsx
+++ frontend/src/components/ProductImage/ProductImage.jsx
@@ -7,52 +7,47 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { searchQuery } from "../../APIs/getSearch/getSearchQuery";
 import HorizontalScrolling from "../HorizontalScrolling/HorinzontalScrolling";
+import SpinnerComponent from "../Spinner/SpinnerComponent";
 
-export default function ProductImage() {
+export default function ProductImage({ productTitle = "" }) {
+  //Create a Context which provides product object
   const [product, setProduct] = useState("");
   const [selectedImage, setSelectedImage] = useState();
-  const [bgBox, setBgBox] = useState("yellow");
 
-  useEffect(() => {
-    (async () => {
-      setProduct(await searchQuery("Tartan"));
-    })();
-  }, []);
-
-  console.log(product);
   return (
     <>
-      {product ? (
-        <Box margin={"70px"} w={"450px"}>
-          <AspectRatio bg="bg.muted" maxW="450px" ratio={1 / 1}>
-            <Image
-              src={selectedImage ? selectedImage : product[0].images[0]}
-              objectFit={"cover"}
+      <Box w={"450px"}>
+        {product ? (
+          <>
+            <AspectRatio bg="bg.muted" maxW="450px" ratio={1 / 1}>
+              <Image
+                src={selectedImage ? selectedImage : product[0].images[0]}
+                objectFit={"cover"}
+              />
+            </AspectRatio>
+            <HorizontalScrolling
+              render={product[0].images.map((imageURL) => {
+                return (
+                  <AspectRatio
+                    ratio={1 / 1}
+                    width={"100px"}
+                    h={"100px"}
+                    bg={imageURL === selectedImage ? "bg.muted" : "none"}
+                  >
+                    <Image
+                      src={imageURL}
+                      onClick={() => setSelectedImage(imageURL)}
+                    />
+                  </AspectRatio>
+                );
+              })}
             />
-          </AspectRatio>
-          <HorizontalScrolling
-            render={product[0].images.map((imageURL) => {
-              return (
-                <AspectRatio
-                  ratio={1 / 1}
-                  width={"100px"}
-                  h={"100px"}
-                  bg={imageURL === selectedImage ? "bg.muted" : "none"}
-                >
-                  <Image
-                    src={imageURL}
-                    onClick={() => setSelectedImage(imageURL)}
-                  />
-                </AspectRatio>
-              );
-            })}
-          />
-        </Box>
-      ) : (
-        <p>Loading</p>
-      )}
+          </>
+        ) : (
+          <SpinnerComponent />
+        )}
+      </Box>
     </>
   );
 }
