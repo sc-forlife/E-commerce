59af86842730cce07aeefc2162fe272d48c9286b
sc-forlife <salemsc7@gmail.com>
2026-06-02 11:48:26 +0200

    Add testing files to the important components

---
+++ frontend/src/components/Card/Card.test.jsx
@@ -0,0 +1,11 @@
+import Display from "./Card";
+import { render } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+
+describe("Display Card", () => {
+  it("Card displays the correct data", () => {
+    render(<Display />);
+
+    expect;
+  });
+});
