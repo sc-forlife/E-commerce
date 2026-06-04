a313ab968bd29e0596611ee58e00dd3f7a14c4e0
sc-forlife <salemsc7@gmail.com>
2026-06-04 06:58:04 +0200

    Created a working testing file for component Card

--- frontend/src/components/Card/Card.test.jsx
+++ frontend/src/components/Card/Card.test.jsx
@@ -1,11 +1,29 @@
 import Display from "./Card";
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { describe, it, expect } from "vitest";
+import { defaultSystem } from "@chakra-ui/react";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Display Card", () => {
-  it("Card displays the correct data", () => {
-    render(<Display />);
-
-    expect;
+  it("Card displays the correct data from arguements", () => {
+    render(
+      <ChakraProvider value={defaultSystem}>
+        <MemoryRouter>
+          <Display
+            title="The dress"
+            price="100"
+            img="http://image.com"
+            alt="Its an image"
+          />
+        </MemoryRouter>
+      </ChakraProvider>,
+    );
+    expect(screen.getByText("The dress")).toBeInTheDocument();
+    expect(screen.getByText("100")).toBeInTheDocument();
+    expect(screen.getByAltText("Its an image")).toBeInTheDocument();
   });
 });
+
+//create a test-utils.jsx file
