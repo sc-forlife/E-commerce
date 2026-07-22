da82cae1b2ceff8d6b017c9ac00da20c005c36be
sc-forlife <salemsc7@gmail.com>
2026-07-22 07:14:49 +0200

    Completed the login_signup component

--- frontend/src/pages/login_signup/login_signup.jsx
+++ frontend/src/pages/login_signup/login_signup.jsx
@@ -9,11 +9,14 @@ import {
   Image,
   HStack,
   Flex,
+  Icon,
 } from "@chakra-ui/react";
 import { useState, createContext } from "react";
 import SignUp from "../../components/SignUp/SignUp";
 import Logo from "../../assets/Logo.png";
 import Login from "../../components/Login/Login";
+import { LuHouse } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 export default function loginSignUp() {
   const [isUser, setIsUser] = useState(true);
@@ -21,6 +24,19 @@ export default function loginSignUp() {
   return (
     <>
       <Flex h={"100vh"} justifyContent={"space-between"} alignItems={"center"}>
+        <Link
+          to={"/"}
+          style={{
+            alignSelf: "flex-start",
+            margin: "0px",
+            position: "absolute",
+          }}
+        >
+          <Button variant={"ghost"}>
+            <Icon as={LuHouse}></Icon>
+            Home
+          </Button>
+        </Link>
         <Center w={"50%"}>
           {isUser ? <Login user={setIsUser} /> : <SignUp user={setIsUser} />}
         </Center>
