cbe06077891b06f0099c27c19f87daf06b047c45
sc-forlife <salemsc7@gmail.com>
2026-07-27 07:08:11 +0200

    Hosting to Github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { Provider } from "./src/components/ui/provider.jsx";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <HashRouter basename="/E-commerce">
+      <Provider>
+        <App />
+      </Provider>
+    </HashRouter>
   </StrictMode>,
 );
