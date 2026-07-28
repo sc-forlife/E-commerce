ac167a3a69900bc4893fee0846f0f526ca34f377
sc-forlife <salemsc7@gmail.com>
2026-07-28 11:24:30 +0200

    NAVBAR Responsive Bar with Modal

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,7 +2,7 @@ import css from "./Navbar.module.css";
 import { useState, useEffect, useEffectEvent, useContext } from "react";
 import { Flex, Box, Button, Image, Icon, Avatar } from "@chakra-ui/react";
 import Logo from "../../assets/Logo.png";
-import LogoMd from "../../assets/SC Logo 1360x760.png";
+import LogoMd from "../../assets/Cropped.png";
 import MenuComponent from "../MenuComponent/MenuComponent.jsx";
 import SearchComponent from "../SearchBar/SearchComponent.jsx";
 import CartComponent from "../CartButton/CartComponent.jsx";
@@ -37,21 +37,16 @@ export default function Navbar() {
           </Link>
         </Flex>
       </Box>
-      <Box
-        w={"100%"}
-        h={"100px"}
-        // bgColor="red.200"
-        m={"5px"}
-        bgImage="url(C:\Users\ThinkPad E480\OneDrive\Desktop\Coding\React Practice\E-commerce\src\assets\SC Logo.png)"
-        bgSize="cover"
-        hideFrom={"md"}
-      >
+      <Box w={"100%"} h={"60px"} m={"5px"} display="flex" showFrom="md">
         <Flex
           align={"center"}
           justify={"space-between"}
           w={"70%"}
           paddingRight={"10px"}
         >
+          <Link to={"/"}>
+            <Image h={"60px"} src={Logo} justify={"flex-start"} m={"5px"} />
+          </Link>
           <MenuComponent />
           <SearchComponent />
         </Flex>
