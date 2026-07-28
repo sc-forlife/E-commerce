4f606b66f1e7bada6023867f4cbdbd4428203a11
sc-forlife <salemsc7@gmail.com>
2026-07-28 07:24:28 +0200

    Creating a responsive NAVBAR

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,6 +2,7 @@ import css from "./Navbar.module.css";
 import { useState, useEffect, useEffectEvent, useContext } from "react";
 import { Flex, Box, Button, Image, Icon, Avatar } from "@chakra-ui/react";
 import Logo from "../../assets/Logo.png";
+import LogoMd from "../../assets/SC Logo 1360x760.png";
 import MenuComponent from "../MenuComponent/MenuComponent.jsx";
 import SearchComponent from "../SearchBar/SearchComponent.jsx";
 import CartComponent from "../CartButton/CartComponent.jsx";
@@ -11,7 +12,7 @@ import { LuUser } from "react-icons/lu";
 export default function Navbar() {
   return (
     <>
-      <Box w={"100%"} h={"60px"} m={"5px"} display="flex">
+      <Box w={"100%"} h={"60px"} m={"5px"} display="flex" hideBelow="md">
         <Flex
           align={"center"}
           justify={"space-between"}
@@ -21,6 +22,36 @@ export default function Navbar() {
           <Link to={"/"}>
             <Image h={"60px"} src={Logo} justify={"flex-start"} m={"5px"} />
           </Link>
+          <MenuComponent />
+          <SearchComponent />
+        </Flex>
+        <Flex align={"center"} justify={"space-around"} w={"30%"}>
+          <Link to={"/Checkout"}>
+            <CartComponent />
+          </Link>
+          <Link to="/login_signup">
+            <Button variant={"ghost"}>Login / Sign Up</Button>
+            <Avatar.Root variant={"solid"}>
+              <Avatar.Fallback name="Guest Account" />
+            </Avatar.Root>
+          </Link>
+        </Flex>
+      </Box>
+      <Box
+        w={"100%"}
+        h={"100px"}
+        // bgColor="red.200"
+        m={"5px"}
+        bgImage="url(C:\Users\ThinkPad E480\OneDrive\Desktop\Coding\React Practice\E-commerce\src\assets\SC Logo.png)"
+        bgSize="cover"
+        hideFrom={"md"}
+      >
+        <Flex
+          align={"center"}
+          justify={"space-between"}
+          w={"70%"}
+          paddingRight={"10px"}
+        >
           <MenuComponent />
           <SearchComponent />
         </Flex>
