8affa28bcac73225f74eab80325dc8e36610e601
sc-forlife <salemsc7@gmail.com>
2026-08-18 07:24:04 +0200

    Checkout sys Responsive design

--- src/pages/checkout_sys/Checkout_sys.jsx
+++ src/pages/checkout_sys/Checkout_sys.jsx
@@ -24,11 +24,13 @@ export default function checkout() {
     <Steps.Root
       defaultStep={0}
       count={steps.length}
-      width={"90%"}
+      width={"100%"}
+      h={"100vh"}
       m={"0px auto"}
       paddingTop={"15px"}
+      justifyContent={"flex-start"}
     >
-      <Steps.List>
+      <Steps.List paddingRight={"20px"} paddingLeft={"20px"}>
         {steps.map((step, index) => (
           <Steps.Item key={index} index={index} title={step.title}>
             <Steps.Indicator />
@@ -39,43 +41,39 @@ export default function checkout() {
       </Steps.List>
       {steps.map((step, index) => (
         <Steps.Content key={index} index={index}>
-          <Center w={"90vw"} h={"85vh"}>
-            <Box
-              bg={"white"}
-              // minW={"800px"}
-              width={"70%"}
-              maxW={"800px"}
-              h={"450px"}
-              borderRadius={"20px"}
-              border={"grey solid 2px"}
-              overflowY={step.title === "Payment" ? "scroll" : null}
-            >
-              {step.description}
-              <Flex justifyContent={"space-around"}>
-                {step.title === "Cart" ? (
-                  <Link to={"/"}>
-                    <Button variant={"ghost"}>
-                      <Icon as={LuHouse}></Icon>
-                      Home
-                    </Button>
-                  </Link>
-                ) : (
-                  <Steps.PrevTrigger asChild>
-                    <Button variant={"ghost"}>
-                      <Icon as={LuArrowLeft}></Icon>
-                      Back
-                    </Button>
-                  </Steps.PrevTrigger>
-                )}
-                <Steps.NextTrigger asChild>
-                  <Button variant={"ghost"}>
-                    Continue <Icon as={LuArrowRight}></Icon>
-                  </Button>
-                </Steps.NextTrigger>
-              </Flex>
-            </Box>
-            {step.extra ? step.extra : ""}
-          </Center>
+          {/* <Box
+          // bg={"white"}
+          // // minW={"800px"}
+          // width={"100%"}
+          // borderRadius={"20px"}
+          // // border={"grey solid 2px"}
+          // overflowY={step.title === "Payment" ? "scroll" : null}
+          > */}
+          {step.description}
+          <Flex justifyContent={"space-around"}>
+            {step.title === "Cart" ? (
+              <Link to={"/"}>
+                <Button variant={"ghost"}>
+                  <Icon as={LuHouse}></Icon>
+                  Home
+                </Button>
+              </Link>
+            ) : (
+              <Steps.PrevTrigger asChild>
+                <Button variant={"ghost"}>
+                  <Icon as={LuArrowLeft}></Icon>
+                  Back
+                </Button>
+              </Steps.PrevTrigger>
+            )}
+            <Steps.NextTrigger asChild>
+              <Button variant={"ghost"}>
+                Continue <Icon as={LuArrowRight}></Icon>
+              </Button>
+            </Steps.NextTrigger>
+          </Flex>
+          {/* </Box> */}
+          {step.extra ? step.extra : ""}
         </Steps.Content>
       ))}
       <Steps.CompletedContent>
